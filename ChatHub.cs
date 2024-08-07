70ee69251ee44b174aca08bf152d439b2198c9ff
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-08-07 12:56:51 +0400

    update

--- ChatHub.cs
+++ ChatHub.cs
@@ -1,30 +1,39 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 
+public class Author
+{
+    public string id { get; set; }
+
+    public string name { get; set; }
+}
+
 public class ChatMessage
 {
-    public string User { get; set; }
+    public Author User { get; set; }
     public string Message { get; set; }
     public DateTime Timestamp { get; set; }
 }
+
+
 
 
 public class ChatHub : Hub
 {
     private static List<ChatMessage> _messages = new List<ChatMessage>();
 
-    public async Task SendMessage(string user, string message)
+    public async Task SendMessage(Author author, string message)
     {
         var chatMessage = new ChatMessage
         {
-            User = user,
+            User = author,
             Message = message,
             Timestamp = DateTime.UtcNow
         };
 
         _messages.Add(chatMessage);
 
-        await Clients.All.SendAsync("ReceiveMessage", user, message);
+        await Clients.All.SendAsync("ReceiveMessage", author, message);
     }
 
 
